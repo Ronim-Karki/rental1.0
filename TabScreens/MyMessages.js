b9e60371bdd7ae5ad4ccda1a8f5a31db8fd27dc5
Ronim123 <70673196+Ronim123@users.noreply.github.com>
2022-02-09 14:25:11 +1100

    my changes

--- TabScreens/MyMessages.js
+++ TabScreens/MyMessages.js
@@ -1,12 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { ScrollView, TextInput } from 'react-native-gesture-handler';
 
 const MyMessages = () => {
   return (
-    <View>
-      <Text>Hello I am Message.</Text>
+    <View style={styles.container}>
+      <ScrollView>
+        <Text>Hello I am Message.</Text>
+        <View style={{ flexDirection: 'row' }}>
+          <TextInput style={styles.box} placeholder="Message" />
+          <Button style={{ height: 20, width: 300 }} title="Send" />
+        </View>
+      </ScrollView>
     </View>
   );
 };
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    alignContent: 'flex-end',
+  },
+  box: {
+    marginTop: 330,
+    backgroundColor: 'white',
+    alignContent: 'flex-end',
+    height: 100,
+    width: 300,
+    flex: 1,
+  },
+});
 export default MyMessages;
